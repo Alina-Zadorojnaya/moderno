15d9c0a939126855c9993699b458696a4c71fe16
Alina-Zadorojna <your_email@whatever.comalinasmartmebel@gmail.com>
2020-05-06 13:25:35 +0300

    'produckt-slider'

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,21 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "13px",
+        readOnly: true
+      });
 
 
+    var mixer = mixitup('.products__inner-box');
+
+    $('.product__slider-inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+        
+
+      });
     
 });
