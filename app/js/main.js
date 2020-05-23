17c5233e22e6df6ca5d2776032a930ff612e672b
Alina-Zadorojna <your_email@whatever.comalinasmartmebel@gmail.com>
2020-05-23 15:16:24 +0300

    'adaptiv-footer-header'

--- app/js/main.js
+++ app/js/main.js
@@ -28,16 +28,21 @@ $(function(){
       $('.icon-th-list').removeClass('active');
     });
 
-    var mixer = mixitup('.products__inner-box');
-
     $('.product__slider-inner').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: false,
         dots: true
-        
-
       });
      
+      $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+      });
+      
+      $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+      });
+
+      var mixer = mixitup('.products__inner-box');
     
 });
