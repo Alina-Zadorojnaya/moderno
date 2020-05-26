cd01cd6e366e32e59d0ae94d1c9bfa64ce1969e9
Alina-Zadorojna <your_email@whatever.comalinasmartmebel@gmail.com>
2020-05-26 19:04:42 +0300

    '404,about,contsct'

--- app/js/main.js
+++ app/js/main.js
@@ -43,6 +43,15 @@ $(function(){
         $('.header__box').toggleClass('active');
       });
 
+      $('.produck-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+          $('.produck-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+          $('.produck-one__tabs .tabs').find('.tab').removeClass('active');
+          $(this).addClass('active');
+          $('#'+id).addClass('active-tab').fadeIn();
+          return false;
+        });
+        
       var mixer = mixitup('.products__inner-box');
     
 });
